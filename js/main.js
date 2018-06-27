e1cd4e58fe70603935dce949013a30f70cbdfe4d
RavAlmasy <eb.vega.m@gmail.com>
2018-06-27 17:36:52 -0400

    avanzando y nada sirve

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
+window.onload = () => {
+  getBip();
+}
+
+
+
 // Botón que maneja la visualización de la barra lateral
-$(document).ready(function() {
-  $('#sidebarCollapse').on('click', function() {
-    $('#sidebar').toggleClass('active');
-  });
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
 });
+
+// Login 
+let mail = document.getElementById('userMail');
+let bipNumber = document.getElementById('bipNumber');
+const bipBtn = document.getElementById('addBtn');
+
+const showBips = () => {
+  bipBtn.addEventListener('submit', () => {
+    document.getElementById('bips').innerHTML+= bipNumber;
+  })
+}
+
+const getBip = () => {
+  fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${bipNumber}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);  
+  })
+}
