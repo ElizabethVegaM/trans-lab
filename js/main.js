be41f612e14d3c2c009d56b84b28ae5d9bcbfb03
RavAlmasy <eb.vega.m@gmail.com>
2018-06-27 15:54:51 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-/* Holiii acá va tu código también */
+// Botón que maneja la visualización de la barra lateral
+$(document).ready(function() {
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar').toggleClass('active');
+  });
+});
